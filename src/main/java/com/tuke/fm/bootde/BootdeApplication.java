b97541c477ad4943f7873f2a4a18c0cd964d8c2e
Administrator <359401754@qq.com>
2019-02-16 10:39:06 +0800

    fir

---
+++ src/main/java/com/tuke/fm/bootde/BootdeApplication.java
@@ -0,0 +1,14 @@
+package com.tuke.fm.bootde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootdeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootdeApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/tuke/fm/bootde/BootdeApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tuke.fm.bootde;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootdeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
